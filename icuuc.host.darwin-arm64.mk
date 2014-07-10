cf459ac932dd2065973bf6cc5abb099007b5672e
Android Chromium Automerger <chromium-automerger@android>
2014-07-10 18:45:24 +0000

    Update makefiles after merge of Chromium at 282385
    This commit was generated by merge_from_chromium.py.
    
    Change-Id: Id3134a781f0a028e54a7dfd5e6978fadd03a89f3

--- icuuc.host.darwin-arm64.mk
+++ icuuc.host.darwin-arm64.mk
@@ -197,7 +197,6 @@ MY_CFLAGS_Debug := \
 	-m64 \
 	-Os \
 	-g \
-	-fomit-frame-pointer \
 	-fdata-sections \
 	-ffunction-sections \
 	-funwind-tables
@@ -273,7 +272,6 @@ MY_CFLAGS_Release := \
 	-fno-ident \
 	-fdata-sections \
 	-ffunction-sections \
-	-fomit-frame-pointer \
 	-funwind-tables
 
 MY_DEFS_Release := \
--- icuuc.host.linux-arm64.mk
+++ icuuc.host.linux-arm64.mk
@@ -198,7 +198,6 @@ MY_CFLAGS_Debug := \
 	-m64 \
 	-Os \
 	-g \
-	-fomit-frame-pointer \
 	-fdata-sections \
 	-ffunction-sections \
 	-funwind-tables
@@ -275,7 +274,6 @@ MY_CFLAGS_Release := \
 	-fno-ident \
 	-fdata-sections \
 	-ffunction-sections \
-	-fomit-frame-pointer \
 	-funwind-tables
 
 MY_DEFS_Release := \
