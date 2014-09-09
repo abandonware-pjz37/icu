5810176f5f189f9a766c7780c5529e6f8676844b
Android Chromium Automerger <chromium-automerger@android>
2014-09-09 14:33:18 +0000

    Update makefiles after merge of Chromium at facf66e09bf8
    This commit was generated by merge_from_chromium.py.
    
    Change-Id: I32f2c35569679571d6864e416ed6b5ab2a6a2ea5

--- icui18n.target.linux-arm.mk
+++ icui18n.target.linux-arm.mk
@@ -205,6 +205,11 @@ MY_CFLAGS_Debug := \
 	-fno-tree-sra \
 	-fno-caller-saves \
 	-Wno-psabi \
+	-fno-partial-inlining \
+	-fno-early-inlining \
+	-fno-tree-copy-prop \
+	-fno-tree-loop-optimize \
+	-fno-move-loop-invariants \
 	-ffunction-sections \
 	-funwind-tables \
 	-g \
@@ -313,6 +318,11 @@ MY_CFLAGS_Release := \
 	-fno-tree-sra \
 	-fno-caller-saves \
 	-Wno-psabi \
+	-fno-partial-inlining \
+	-fno-early-inlining \
+	-fno-tree-copy-prop \
+	-fno-tree-loop-optimize \
+	-fno-move-loop-invariants \
 	-ffunction-sections \
 	-funwind-tables \
 	-g \
